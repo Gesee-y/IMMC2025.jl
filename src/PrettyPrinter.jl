a8c7a58f0e32f8f8e8febc62dfc724d22ab9039c
Talom Laël <gesee37@gmail.com>
2025-04-13 16:24:33 +0100

    Update PrettyPrinter.jl

--- src/PrettyPrinter.jl
+++ src/PrettyPrinter.jl
@@ -4,8 +4,8 @@ function print_schedule(io,teams_names,sched)
     str = ""
     d = size(sched)
 
-    d2 = div(d[2],2)
-    d1 = div(d[1],2)
+    d2 = div(d[2],1)
+    d1 = div(d[1],1)
     
     for k in Base.OneTo(d[3])
         str = str * " \n Day $k \n"
@@ -18,5 +18,39 @@ function print_schedule(io,teams_names,sched)
         end
     end
 
+    println(io,sched)
+    println(io,str)
+end
+
+
+function print_schedule(io,teams_names,sched::Dict,L,K,n)
+    str = ""
+    
+    idx = 1
+    cnt = 0
+
+    str = str * " \n Day $idx \n"
+
+    for k in Base.OneTo(K)
+
+        for j in Base.OneTo(L)
+            for i in Base.OneTo(L)
+                if haskey(sched,((i,j),k))
+                    if cnt < n
+                        str = str * teams_names[i] * " vs " * teams_names[j] * "\n"
+                        cnt += 1
+                    else
+                        idx +=1
+                        str = str * " \n Day $idx \n"
+                        cnt = 0
+                    end
+                end
+            end
+        end
+
+        idx += 1
+    end
+
+    println(io,sched)
     println(io,str)
 end
